ddabe8e229ac9a5f23c4662f1c1fc85df30632a3
Joseph Lumbley <joelumbley16@gmail.com>
2024-12-19 10:09:20 -0600

    Add project files.

---
+++ Draw Arc/Form1.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Timer1 = New Timer(components)
+        StartAngleTrackBar = New TrackBar()
+        StartAngleLabel = New Label()
+        SweepAngleTrackBar = New TrackBar()
+        SweepAngleLabel = New Label()
+        CType(StartAngleTrackBar, ComponentModel.ISupportInitialize).BeginInit()
+        CType(SweepAngleTrackBar, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Timer1
+        ' 
+        ' 
+        ' StartAngleTrackBar
+        ' 
+        StartAngleTrackBar.BackColor = SystemColors.Control
+        StartAngleTrackBar.Location = New Point(267, 263)
+        StartAngleTrackBar.Maximum = 360
+        StartAngleTrackBar.Minimum = -360
+        StartAngleTrackBar.Name = "StartAngleTrackBar"
+        StartAngleTrackBar.Size = New Size(479, 69)
+        StartAngleTrackBar.TabIndex = 0
+        StartAngleTrackBar.TickFrequency = 3
+        StartAngleTrackBar.TickStyle = TickStyle.TopLeft
+        ' 
+        ' StartAngleLabel
+        ' 
+        StartAngleLabel.AutoSize = True
+        StartAngleLabel.BackColor = SystemColors.Control
+        StartAngleLabel.ForeColor = SystemColors.ControlText
+        StartAngleLabel.Location = New Point(348, 293)
+        StartAngleLabel.Name = "StartAngleLabel"
+        StartAngleLabel.Size = New Size(63, 25)
+        StartAngleLabel.TabIndex = 2
+        StartAngleLabel.Text = "Label1"
+        ' 
+        ' SweepAngleTrackBar
+        ' 
+        SweepAngleTrackBar.BackColor = SystemColors.Control
+        SweepAngleTrackBar.Location = New Point(169, 52)
+        SweepAngleTrackBar.Maximum = 360
+        SweepAngleTrackBar.Minimum = -360
+        SweepAngleTrackBar.Name = "SweepAngleTrackBar"
+        SweepAngleTrackBar.Size = New Size(479, 69)
+        SweepAngleTrackBar.TabIndex = 1
+        SweepAngleTrackBar.TickFrequency = 3
+        SweepAngleTrackBar.TickStyle = TickStyle.TopLeft
+        SweepAngleTrackBar.Value = 45
+        ' 
+        ' SweepAngleLabel
+        ' 
+        SweepAngleLabel.AutoSize = True
+        SweepAngleLabel.BackColor = SystemColors.Control
+        SweepAngleLabel.ForeColor = SystemColors.ControlText
+        SweepAngleLabel.Location = New Point(291, 82)
+        SweepAngleLabel.Name = "SweepAngleLabel"
+        SweepAngleLabel.Size = New Size(63, 25)
+        SweepAngleLabel.TabIndex = 3
+        SweepAngleLabel.Text = "Label1"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(SweepAngleLabel)
+        Controls.Add(StartAngleLabel)
+        Controls.Add(StartAngleTrackBar)
+        Controls.Add(SweepAngleTrackBar)
+        Name = "Form1"
+        Text = "Form1"
+        CType(StartAngleTrackBar, ComponentModel.ISupportInitialize).EndInit()
+        CType(SweepAngleTrackBar, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents StartAngleTrackBar As TrackBar
+    Friend WithEvents SweepAngleTrackBar As TrackBar
+    Friend WithEvents StartAngleLabel As Label
+    Friend WithEvents SweepAngleLabel As Label
+
+End Class
---
+++ Draw Arc/Form1.vb
@@ -0,0 +1,395 @@
+﻿' Draw Arc
+
+' MIT License
+' Copyright(c) 2024 Joseph W. Lumbley
+
+' Permission Is hereby granted, free Of charge, to any person obtaining a copy
+' of this software And associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, And/Or sell
+' copies of the Software, And to permit persons to whom the Software Is
+' furnished to do so, subject to the following conditions:
+
+' The above copyright notice And this permission notice shall be included In all
+' copies Or substantial portions of the Software.
+
+' THE SOFTWARE Is PROVIDED "AS IS", WITHOUT WARRANTY Of ANY KIND, EXPRESS Or
+' IMPLIED, INCLUDING BUT Not LIMITED To THE WARRANTIES Of MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE And NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS Or COPYRIGHT HOLDERS BE LIABLE For ANY CLAIM, DAMAGES Or OTHER
+' LIABILITY, WHETHER In AN ACTION Of CONTRACT, TORT Or OTHERWISE, ARISING FROM,
+' OUT OF Or IN CONNECTION WITH THE SOFTWARE Or THE USE Or OTHER DEALINGS IN THE
+' SOFTWARE.
+
+' https://github.com/JoeLumbley/Timer
+
+Imports System.ComponentModel
+Imports System.Drawing.Drawing2D
+'Imports System.IO
+Imports System.Runtime.InteropServices
+Imports System.Text
+
+Public Class Form1
+
+    Private Context As BufferedGraphicsContext
+    Private Buffer As BufferedGraphics
+
+    Private Structure DisplayObject
+        Public Location As Point
+        Public Text As String
+        Public Font As Font
+    End Structure
+
+    Private Structure ButtonStruct
+        Public Rect As Rectangle
+        Public Radius As Integer
+        Public Text As String
+        Public TextLocation As Point
+        Public Font As Font
+    End Structure
+
+    Private CodeDisplay As DisplayObject
+
+    Private CircleOfProgress As Rectangle
+
+    Private CircleOfProgressPen As Pen
+
+    Private CircleOfProgressBackgroundPen As Pen
+
+    ' Define the start angle in degrees at 0 or 3 o'clock.
+    Private Const startAngle As Single = 0.0F
+
+    Private sweepAngle As Single
+
+    Private ReadOnly AlineCenterMiddle As New StringFormat With {.Alignment = StringAlignment.Center, .LineAlignment = StringAlignment.Center}
+
+    Private ReadOnly EmojiFont As New Font("Segoe UI Emoji", 25)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        InitializeForm()
+
+        InitializeBuffer()
+
+        Timer1.Interval = 15
+
+        Timer1.Enabled = True
+
+        Debug.Print($"Program running... {Now.ToShortTimeString}")
+
+        StartAngleLabel.Text = $"Start Angle: {StartAngleTrackBar.Value}°"
+
+        SweepAngleLabel.Text = $"Sweep Angle: {SweepAngleTrackBar.Value}°"
+
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+
+        If Not WindowState = FormWindowState.Minimized Then
+
+            ResizeCodeDisplay()
+
+            ResizeControls()
+
+            ResizeCircleOfProgress()
+
+            DisposeBuffer()
+
+        End If
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        UpdateCodeDisplay()
+
+        If Not WindowState = FormWindowState.Minimized Then
+
+            Refresh() ' Calls OnPaint Sub
+
+        End If
+
+    End Sub
+
+    Protected Overrides Sub OnPaint(ByVal e As PaintEventArgs)
+
+        AllocateBuffer(e)
+
+        DrawDisplays()
+
+        Buffer.Render(e.Graphics)
+
+        MyBase.OnPaint(e)
+
+    End Sub
+
+    Protected Overrides Sub OnPaintBackground(ByVal e As PaintEventArgs)
+
+        'Intentionally left blank. Do not remove.
+
+    End Sub
+
+    Private Sub AllocateBuffer(e As PaintEventArgs)
+
+        ' Allocate the buffer if it hasn't been allocated yet
+        If Buffer Is Nothing Then
+
+            Buffer = Context.Allocate(e.Graphics, ClientRectangle)
+
+            With Buffer.Graphics
+
+                .CompositingMode = Drawing2D.CompositingMode.SourceOver
+                .TextRenderingHint = Drawing.Text.TextRenderingHint.AntiAlias
+                .SmoothingMode = Drawing2D.SmoothingMode.HighQuality
+                .PixelOffsetMode = Drawing2D.PixelOffsetMode.HighQuality
+                .CompositingQuality = Drawing2D.CompositingQuality.HighQuality
+                .InterpolationMode = InterpolationMode.HighQualityBicubic
+                .TextContrast = SmoothingMode.HighQuality
+
+            End With
+
+        End If
+
+    End Sub
+
+    Private Sub DisposeBuffer()
+
+        If Buffer IsNot Nothing Then
+
+            Buffer.Dispose()
+
+            Buffer = Nothing ' Set to Nothing to avoid using a disposed object
+
+            ' The buffer will be reallocated in OnPaint
+
+        End If
+
+    End Sub
+
+    Private Sub DrawDisplays()
+
+        If Buffer IsNot Nothing Then
+
+            Try
+
+                With Buffer.Graphics
+
+                    .Clear(SystemColors.Control)
+
+                    .DrawEllipse(CircleOfProgressBackgroundPen, CircleOfProgress)
+
+                    .DrawArc(CircleOfProgressPen, CircleOfProgress, StartAngleTrackBar.Value, SweepAngleTrackBar.Value)
+
+                    .DrawString(CodeDisplay.Text, CodeDisplay.Font, New SolidBrush(SystemColors.ControlText), CodeDisplay.Location, AlineCenterMiddle)
+
+                End With
+
+            Catch ex As Exception
+
+                Debug.Print("Draw error: " & ex.Message)
+
+            End Try
+
+        Else
+
+            Debug.Print("Buffer is not initialized.")
+
+        End If
+
+    End Sub
+
+    Private Sub UpdateCodeDisplay()
+
+        CodeDisplay.Text = $".DrawArc(Pen, Rect, {StartAngleTrackBar.Value}, {SweepAngleTrackBar.Value})"
+
+    End Sub
+
+    Private Sub InitializeForm()
+
+        CenterToScreen()
+
+        SetStyle(ControlStyles.UserPaint, True)
+
+        SetStyle(ControlStyles.OptimizedDoubleBuffer, True)
+
+        SetStyle(ControlStyles.AllPaintingInWmPaint, True)
+
+        Text = "Draw Arc - Code with Joe"
+
+        Me.WindowState = FormWindowState.Maximized
+
+    End Sub
+
+    Private Sub InitializeBuffer()
+
+        ' Set context to the context of this app.
+        Context = BufferedGraphicsManager.Current
+
+        Context.MaximumBuffer = Screen.PrimaryScreen.WorkingArea.Size
+
+        ' Allocate the buffer initially using the current client rectangle
+        Buffer = Context.Allocate(CreateGraphics(), ClientRectangle)
+
+        With Buffer.Graphics
+
+            .CompositingMode = Drawing2D.CompositingMode.SourceOver
+            .TextRenderingHint = Drawing.Text.TextRenderingHint.AntiAlias
+            .SmoothingMode = Drawing2D.SmoothingMode.HighQuality
+            .PixelOffsetMode = Drawing2D.PixelOffsetMode.None
+            .CompositingQuality = Drawing2D.CompositingQuality.HighQuality
+
+        End With
+
+    End Sub
+
+    Private Sub ResizeCodeDisplay()
+
+        Dim FontSize As Single
+
+        If ClientSize.Height / 35 > 10 Then
+
+            FontSize = ClientSize.Height / 35
+
+        Else
+
+            FontSize = 10
+
+        End If
+
+        Dim ButtonSize As Integer
+
+        If ClientSize.Height / 14 > 32 Then
+
+            ButtonSize = ClientSize.Height / 14
+
+        Else
+
+            ButtonSize = 32
+
+        End If
+
+        CodeDisplay.Font = New Font("Segoe UI",
+                                       FontSize,
+                                       FontStyle.Regular)
+
+        CodeDisplay.Location.X = ClientSize.Width / 2
+
+        CodeDisplay.Location.Y = ClientSize.Height / 2 - ButtonSize * 6
+
+    End Sub
+
+    Private Sub ResizeControls()
+
+        Dim FontSize As Single
+
+        If ClientSize.Height / 45 > 10 Then
+
+            FontSize = ClientSize.Height / 45
+
+        Else
+
+            FontSize = 10
+
+        End If
+
+        Dim ButtonSize As Integer
+
+        If ClientSize.Height / 14 > 32 Then
+
+            ButtonSize = ClientSize.Height / 14
+
+        Else
+
+            ButtonSize = 32
+
+        End If
+
+        StartAngleTrackBar.Width = ClientSize.Width / 4.1
+
+        StartAngleTrackBar.Left = ClientSize.Width / 2 - StartAngleTrackBar.Width / 2
+
+        StartAngleTrackBar.Top = ClientSize.Height / 2 - StartAngleTrackBar.Height
+
+        StartAngleLabel.Left = StartAngleTrackBar.Left + 8
+
+        StartAngleLabel.Top = StartAngleTrackBar.Top + 40
+
+        SweepAngleTrackBar.Width = ClientSize.Width / 4.1
+
+        SweepAngleTrackBar.Left = ClientSize.Width / 2 - SweepAngleTrackBar.Width / 2
+
+        SweepAngleTrackBar.Top = ClientSize.Height / 2 + SweepAngleTrackBar.Height * 0.1
+
+        SweepAngleLabel.Left = SweepAngleTrackBar.Left + 8
+
+        SweepAngleLabel.Top = SweepAngleTrackBar.Top + 40
+
+    End Sub
+
+    Private Sub ResizeCircleOfProgress()
+
+        Dim PenSize As Single
+
+        If ClientSize.Height / 30 > 15 Then
+
+            PenSize = ClientSize.Height / 30
+
+        Else
+
+            PenSize = 15
+
+        End If
+
+        CircleOfProgressBackgroundPen = New Pen(SystemColors.ControlLightLight, PenSize)
+
+        If ClientSize.Height / 40 > 10 Then
+
+            PenSize = ClientSize.Height / 40
+
+        Else
+
+            PenSize = 10
+
+        End If
+
+        CircleOfProgressPen = New Pen(SystemColors.MenuHighlight, PenSize) With {
+            .EndCap = LineCap.Round,
+            .StartCap = LineCap.Round
+        }
+
+        Dim CircleSize As Single
+
+        If ClientSize.Height / 1.5 > 300 Then
+
+            CircleSize = ClientSize.Height / 1.5
+
+        Else
+
+            CircleSize = 300
+
+        End If
+
+        CircleOfProgress.Width = CircleSize
+        CircleOfProgress.Height = CircleSize
+
+        CircleOfProgress.X = ClientSize.Width / 2 - CircleOfProgress.Width / 2
+        CircleOfProgress.Y = ClientSize.Height / 2 - CircleOfProgress.Height / 2
+
+    End Sub
+
+    Private Sub StartAngleTrackBar_Scroll(sender As Object, e As EventArgs) Handles StartAngleTrackBar.Scroll
+
+        StartAngleLabel.Text = $"Start Angle: {StartAngleTrackBar.Value}°"
+
+    End Sub
+
+    Private Sub SweepAngleTrackBar_Scroll(sender As Object, e As EventArgs) Handles SweepAngleTrackBar.Scroll
+
+        SweepAngleLabel.Text = $"Sweep Angle: {SweepAngleTrackBar.Value}°"
+
+    End Sub
+
+End Class
+
+' Monica is our an AI assistant.
+' https://monica.im/
